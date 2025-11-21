caafb5de4457236837383859df6722166dac24a0
«movavok» <pkurishchenkoukr.net05@gmail.com>
2025-11-21 16:00:38 +0200

    Added start game logic and collision of entities

--- ClashOfTanks1990/game/game.cpp
+++ ClashOfTanks1990/game/game.cpp
@@ -1,3 +1,47 @@
 #include "game.h"
 
-Game::Game() {}
+Game::Game() {
+    player = new PlayerTank(QPointF(100, 100), 30, 30, 100.0f);
+    addEntity(player);
+
+    for (int i = 0; i < 3; i++) {
+        EnemyTank* enemy = new EnemyTank(QPointF(200 + i * 60, 200), 30, 30, 70.0f);
+        addEntity(enemy);
+    }
+}
+
+void Game::addEntity(Entity* e) { entities.append(e); }
+
+void Game::removeEntity(Entity* e)
+{
+    entities.removeOne(e);
+    delete e;
+}
+
+void Game::update(float deltaTime) {
+    for (Entity* entity : entities) {
+        QPointF oldPos = entity->getPosition();
+
+        entity->update(deltaTime);
+
+        if (checkCollision(entity)) entity->setPosition(oldPos); //otkat
+    }
+
+    for (int index = entities.size() - 1; index >= 0; --index)
+        if (!entities[index]->isAlive()) removeEntity(entities[index]);
+}
+
+bool Game::checkCollision(Entity* entity) {
+    QRectF eBounds = entity->bounds();
+    for (Entity* other : entities) {
+        if (other == entity) continue;
+        if (eBounds.intersects(other->bounds())) return true;
+    }
+    return false;
+}
+
+void Game::render(QPainter* painter) { for (Entity* entity : entities) entity->render(painter); }
+
+void Game::handleKeyPress(Qt::Key key) { if (player) player->handleKeyPress(key); }
+
+void Game::handleKeyRelease(Qt::Key key) { if (player) player->handleKeyRelease(key); }
--- ClashOfTanks1990/game/game.h
+++ ClashOfTanks1990/game/game.h
@@ -1,10 +1,30 @@
 #ifndef GAME_H
 #define GAME_H
 
+#include <QList>
+#include <QPainter>
+#include "../entities/entity.h"
+#include "../entities/playertank.h"
+#include "../entities/enemytank.h"
+
 class Game
 {
 public:
     Game();
+
+    void update(float);
+    bool checkCollision(Entity*);
+    void render(QPainter*);
+
+    void handleKeyPress(Qt::Key);
+    void handleKeyRelease(Qt::Key);
+
+    void addEntity(Entity*);
+    void removeEntity(Entity*);
+
+private:
+    PlayerTank* player;
+    QList<Entity*> entities;
 };
 
 #endif // GAME_H
