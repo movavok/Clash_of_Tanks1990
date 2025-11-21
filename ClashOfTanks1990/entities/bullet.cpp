d1cb15154068cce0291313987c43bdbecc19ec22
«movavok» <pkurishchenkoukr.net05@gmail.com>
2025-11-21 19:55:46 +0200

    Changed a lot of abstract classes to work with bullets

--- ClashOfTanks1990/entities/bullet.cpp
+++ ClashOfTanks1990/entities/bullet.cpp
@@ -1,3 +1,24 @@
 #include "bullet.h"
 
-Bullet::Bullet() {}
+Bullet::Bullet(const QPointF& pos, Tank::Direction dir, float spd, Tank* own)
+    : Entity(pos), direction(dir), speed(spd), owner(own), size(5) {}
+
+void Bullet::update(float deltaTime) {
+    float distance = speed * deltaTime;
+
+    switch(direction) {
+    case Tank::UP:    position.ry() -= distance; break;
+    case Tank::DOWN:  position.ry() += distance; break;
+    case Tank::LEFT:  position.rx() -= distance; break;
+    case Tank::RIGHT: position.rx() += distance; break;
+    }
+}
+
+void Bullet::render(QPainter* painter) {
+    painter->setBrush(Qt::yellow);
+    painter->drawEllipse(bounds());
+}
+
+Tank* Bullet::getOwner() const { return owner; }
+
+QRectF Bullet::bounds() const { return QRectF(position.x(), position.y(), size, size); }
--- ClashOfTanks1990/entities/bullet.h
+++ ClashOfTanks1990/entities/bullet.h
@@ -1,12 +1,24 @@
 #ifndef BULLET_H
 #define BULLET_H
 
-#include "entity.h"
+#include "tank.h"
 
 class Bullet : public Entity
 {
 public:
-    Bullet();
+    Bullet(const QPointF&, Tank::Direction, float, Tank*);
+
+    void update(float) override;
+    void render(QPainter*) override;
+    QRectF bounds() const override;
+
+    Tank* getOwner() const;
+
+private:
+    Tank* owner;
+    Tank::Direction direction;
+    float speed;
+    unsigned short size;
 };
 
 #endif // BULLET_H
